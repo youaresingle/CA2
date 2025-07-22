a78d1a13f0b5cc13aca9eebbe178fed5996ba78e
youaresingle <shelinyap9@gmail.com>
2025-07-22 20:49:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
+const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session');
+const flash = require('connect-flash');
+const multer = require('multer');
+const app = express();
 
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images'); // Directory to save uploaded files
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname); 
+    }
+});
+
+const upload = multer({ storage: storage });
+
+const connection = mysql.createConnection({
+    host: '7-aytn.h.filess.io',
+    user: 'CA2_duckmoment',
+    password: '859ba7ea708188fb6fa493d69a28b1f41785baa3',
+    database: 'CA2_duckmoment'
+  });
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+// Set up view engine
+app.set('view engine', 'ejs');
+//  enable static files
+app.use(express.static('public'));
+// enable form processing
+app.use(express.urlencoded({
+    extended: false
+}));
+
+//TO DO: Insert code for Session Middleware below 
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    // Session expires after 1 week of inactivity
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 } 
+}));
+
+app.use(flash());
